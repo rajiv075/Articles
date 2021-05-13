c10baa3d2833fcfb53d57de0e2936c9aa5b56b9b
Rajeev G <rajiv.iiitb@gmail.com>
2021-05-13 23:25:05 +0530

    Unit Test Cases added

--- server/test/mo_createUserTest.js
+++
@@ -1,8 +0,0 @@
-// const { assert } = require("assert");
-// const User = require("../models/User");
-
-// describe("Create Users", () => {
-//   it("Create User in Db", () => {
-//     assert(true);
-//   });
-// });
--- server/test/test.js
+++ server/test/test.js
@@ -1,83 +1,58 @@
 const app = require("../server");
-
+const logger = require("../logger");
 const request = require("supertest");
 
-const auth = require("../middleware/auth");
+request();
+let token;
+
+// const auth = require("../middleware/auth");
 
 // const logger = require("../logger");
 const baseUrl = "http://localhost:5000/api/auth";
 
-let token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXN1bHQiOnsiaWQiOjUsImZpcnN0bmFtZSI6ImZpcnN0bmFtZSIsImxhc3RuYW1lIjoibGFzdG5hbWUiLCJlbWFpbCI6ImVtYWlsMUBnbWFpbC5jb20ifSwiaWF0IjoxNjE5NzgyOTkwLCJleHAiOjE2MTk4MDA5OTB9.Zsh_ZrMMiaWFl59WFUEfTEueFA2ngcQ3_t2WrYLeBn4";
-let id = 5;
-
-// Signup
-// describe("\n\n\n\nSignup ::", () => {
-//   it("Create User Success\n\n", (done) => {
-//     request(baseUrl)
-//       .post("/signup")
-//       .send({
-//         firstname: "firstname ",
-//         lastname: "lastname",
-//         email: "email@gmail.com",
-//         password: "password",
-//         role: "doctor",
-//       })
-//       .end((err, res) => {
-//         if (res.body.success === 0) {
-//           logger.error(err);
-//           throw err;
-//         }
-//         if (res.body.success === 1) {
-//           console.log(res.body);
-//           logger.info(res.body);
-//         }
-
-//         done();
-//       });
-//   });
-
-//   it("Failed to signup \n\n", (done) => {
-//     request(baseUrl)
-//       .post("/signup")
-//       .send({
-//         firstname: "firstname",
-//         lastname: "lastname",
-//         email: "email@gmail.com",
-//         password: "password",
-//         role: "doctor",
-//       })
-//       .end((err, res) => {
-//         if (res.body.success === 1) {
-//           logger.error(err);
-//           throw err;
-//         }
-//         if (res.body.success === 0) {
-//           console.log(res.body);
-//           logger.info(res.body);
-//         }
-//         done();
-//       });
-//   });
-// });
-
-// Login
-describe("\n\n\n\nLogin test :: ", () => {
-  it("should not able log in \n\n", (done) => {
-    request(baseUrl)
+// Signup;
+describe("\n\n\n\nSignup ::", () => {
+  it("Failed to signup \n\n", (done) => {
+    request("http://localhost:5000/api/users")
       .post("/")
       .send({
-        email: "rajiv075@gmail.com",
-        password: "rajiv075",
+        name: "firstname",
+        email: "email@gmail.com",
+        password: "password",
       })
       .end((err, res) => {
         if (res.body.success === 1) {
-          //   logger.error(err);
+          logger.error(err);
           throw err;
         }
         if (res.body.success === 0) {
           console.log(res.body);
-          //   logger.info(res.body);
+          logger.info(res.body);
+          token = res.body.token;
+        }
+        done();
+      });
+  });
+});
+
+// Login
+
+describe("\n\n\n\nLogin test :: ", () => {
+  it("should not be able log in \n\n", (done) => {
+    request(baseUrl)
+      .post("/")
+      .send({
+        email: "rajiv075@gmail.com",
+        password: "rajiv",
+      })
+      .end((err, res) => {
+        console.log(res.body.success);
+        if (res.body.msg == "Invalid Credentials") {
+          logger.info(res.body.msg);
+        } else {
+          // console.log(res.body);
+          // logger.info(res.body);
+          console.log("logged in");
         }
         done();
       });
@@ -91,60 +66,46 @@ describe("\n\n\n\nLogin test :: ", () => {
         password: "rajiv075",
       })
       .end((err, res) => {
-        if (res.body.success === 0) {
-          //   logger.error(err);
+        // console.log(res.body);
+        if (res.body.sucess === 0) {
+          logger.error(err);
           throw err;
         }
-        if (res.body.success === 1) {
-          console.log(res.body);
-          //   logger.info(res.body);
-          //   token = res.body.currentUser.token;
-          //   id = res.body.currentUser.id;
+        if (res.body) {
+          // console.log(res.body);
+          logger.info("User token :" + res.body.token);
+          token = res.body.token;
         }
         done();
       });
   });
 });
 
-// Get all users
-// describe("\n\n\n\nGet all user :: ", () => {
-//   it("Fetch all the users\n\n", (done) => {
-//     request(baseUrl)
-//       .get("/getallusers")
-//       .set("Authorization", `Bearer ${token}`)
-//       .end((err, res) => {
-//         if (res.body.success === 0) {
-//           logger.error(err);
-//           throw err;
-//         }
-//         if (res.body.success === 0) {
-//           console.log(res.body);
-//           logger.info(res.body);
-//         }
-//         done();
-//       });
-//   });
-// });
-
-// Delete users
-// describe("\n\n\n\nDelete User ::", () => {
-//   it("Delete User Success\n\n", (done) => {
-//     request(baseUrl)
-//       .delete("/")
-//       .set("Authorization", `Bearer ${token}`)
-//       .send({
-//         id: `${id}`,
-//       })
-//       .end((err, res) => {
-//         if (res.body.success === 0) {
-//           logger.error(err);
-//           throw err;
-//         }
-//         if (res.body.success === 1) {
-//           console.log(res.body);
-//           logger.info(res.body);
-//         }
-//         done();
-//       });
-//   });
-// });
+// Get all articles
+describe("\n\n\n\nGet all articles :: ", () => {
+  it("Cannot Fetch  the articles\n\n", (done) => {
+    request("http://localhost:5000/api/contacts")
+      .get("/")
+      .end((err, res) => {
+        // console.log(res.body);
+        if ((res.body.msg = "No token, authorization denied")) {
+          logger.info("Authorization Error");
+        }
+        done();
+      });
+  });
+  it("Fetch all the articles\n\n", (done) => {
+    request("http://localhost:5000/api/contacts")
+      .get("/")
+      .set({ token })
+      .end((err, res) => {
+        // console.log(res.body);
+        if ((res.body.msg = "No token, authorization denied")) {
+          logger.info("Authorization Error");
+        } else {
+          console.log(res.body);
+        }
+        done();
+      });
+  });
+});
